b4dc9177536caad58f55d434294869a89129ffe6
rizky anugrah <rizkyanugrah51963@gmail.com>
2025-11-14 15:00:08 +0700

    users

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -21,15 +21,15 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
 
     <!-- Libraries Stylesheet -->
-    <link href="lib/animate/animate.min.css" rel="stylesheet">
-    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
-    <link href="lib/lightbox/css/lightbox.min.css" rel="stylesheet">
+    <link href="{{ asset('user/assets/lib/animate/animate.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('user/assets/lib/owlcarousel/assets/owl.carousel.min.css')}}" rel="stylesheet">
+    <link href="{{ asset('user/assets/lib/lightbox/css/lightbox.min.css')}}" rel="stylesheet">
 
     <!-- Customized Bootstrap Stylesheet -->
-    <link rel="stylesheet" href="{{ asset('user/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('user/assets/css/bootstrap.min.css') }}">
 
     <!-- Template Stylesheet -->
-   <link rel="stylesheet" href="{{ asset('user/css/style.css') }}">
+   <link rel="stylesheet" href="{{ asset('user/assets/css/style.css') }}">
 </head>
 
 <body>
@@ -548,15 +548,15 @@
     <!-- JavaScript Libraries -->
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="lib/wow/wow.min.js"></script>
-    <script src="lib/easing/easing.min.js"></script>
-    <script src="lib/waypoints/waypoints.min.js"></script>
-    <script src="lib/owlcarousel/owl.carousel.min.js"></script>
-    <script src="lib/isotope/isotope.pkgd.min.js"></script>
-    <script src="lib/lightbox/js/lightbox.min.js"></script>
+    <script src="/user/assets/lib/wow/wow.min.js"></script>
+    <script src="/user/assets/lib/easing/easing.min.js"></script>
+    <script src="/user/assets/lib/waypoints/waypoints.min.js"></script>
+    <script src="/user/assets/lib/owlcarousel/owl.carousel.min.js"></script>
+    <script src="/user/assets/lib/isotope/isotope.pkgd.min.js"></script>
+    <script src="/user/assets/lib/lightbox/js/lightbox.min.js"></script>
 
     <!-- Template Javascript -->
-    <script src="js/main.js"></script>
+    <script src="user/assets/js/main.js"></script>
 </body>
 
 </html>
